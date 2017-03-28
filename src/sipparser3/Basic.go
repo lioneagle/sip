d5970a2dc502df2298b8f653cc7db137ae619415
lioneagle <leoeagle@163.com>
2017-03-29 00:14:50 +0800

    add abnf

--- src/sipparser3/Basic.go
+++ src/sipparser3/Basic.go
@@ -2,36 +2,10 @@ package sipparser3
 
 import (
 	"bytes"
-	"fmt"
 	"reflect"
 	"strconv"
 	"unsafe"
 )
-
-type AbnfIsInCharset func(ch byte) bool
-
-type AbnfEncoder interface {
-	Encode(buf *bytes.Buffer)
-}
-
-func AbnfEncoderToString(encoder AbnfEncoder) string {
-	var buf bytes.Buffer
-	encoder.Encode(&buf)
-	return buf.String()
-}
-
-type AbnfError struct {
-	description string
-	src         []byte
-	pos         int
-}
-
-func (err *AbnfError) Error() string {
-	if err.pos < len(err.src) {
-		return fmt.Sprintf("%s at src[%d]: %s", err.description, err.pos, string(err.src[err.pos:]))
-	}
-	return fmt.Sprintf("%s at end", err.description)
-}
 
 func Str2bytes(s string) []byte {
 	//h := reflect.SliceHeader{Data: uintptr(unsafe.Pointer(&s)), Len: len(s), Cap: len(s)}
